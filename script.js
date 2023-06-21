adc1a91845e1e03a7a5ff635fd71f096be4721a6
Luděk Roleček <ludek@rolecek.cz>
2023-06-21 17:54:42 +0200

    Aktualizace podkladů

---
+++ script.js
@@ -0,0 +1,121 @@
+console.log('JS funguje');
+
+// Máš toto pole jmen.
+let jmena = ['Jana', 'Emil', 'Lenka', 'Alena', 'Karel', 'Pavla'];
+
+console.log('Výchozí seznam jmen:');
+console.log(jmena);
+
+/*
+1) Vypiš třetí jméno v pořadí.
+Nezapomeň, že pole se čísluje on 0
+*/
+
+
+/*
+2) Použij stejné pole jmen z prvního příkladu.
+Kolik jmen je celkem v poli?
+*/
+
+
+/*
+3) Přidej na konec pole jména Helena a Arnošt.
+*/
+
+
+/*
+4) Přidej na začátek jméno Kamila a potm ještě Libor.
+*/
+
+
+/*
+5) Odeber z pole první a poslední jméno a vypiš
+obě odebraná jména a pak i výsledné pole.
+*/
+
+
+/*
+6) Odeber čtvrté jméno.
+Odebrané jméno i upravené pole vypiš.
+*/
+
+
+/*
+7) Třetí jméno v seznamu nahraď jmény Lucie a Hana.
+Odebrané jméno i upravené pole vypiš.
+*/
+
+
+/*
+8) Najdi v poli Alenu a smaž ji.
+Upravené pole vypiš.
+*/
+
+
+
+/* --- cykly --- */
+
+/*
+9) Vypiš do konzole všechny prvky pole jmen ve tvaru:
+0 - Jana
+1 - Emil
+atd.
+Číslo na začátku je index prvku v poli.
+*/
+
+
+/*
+10) Použij stejné pole jmen ale tentokrát vypiš
+pouze jména na lichých indexech.
+Pamatuješ na operátor %, který vrací zbytek po
+celočíselném dělení? Např. 5 % 2 === 1
+*/
+
+
+/*
+11) Máš dvě pole. V jednom jsou jména zaměstnanců
+a ve druhém jsou jejich platy. Indexy v obou polích
+si odpovídají (tj. osoba na indexu 0 má plat také
+na indexu 0).
+Vypiš, kolik kdo vydělává, ať víme, komu závidět.
+Ve formátu:
+Jarmila: 27500 Kč
+*/
+let zamestnanci = ['Jarmila', 'Bedřich', 'Zuzana', 'Eliška', 'Petr'];
+let platy = [27500, 34900, 48000, 32000, 18000];
+
+
+
+/*
+12) Mít hodnoty v oddělených polích je pěkná blbost, co?
+Programu je to celkem jedno, ale pro lidi je na první pohled
+špatně vidět, komu patří která hodnota v druhém poli.
+V poli ale můžou být i objekty!
+Objekt může být osoba, která má vlastnosti jméno, věk, plat.
+Vypiš znovu jména a vedle plat pracovníka.
+Jarmila: 27500 Kč
+*/
+let pracovnici = [
+	{ jmeno: 'Jarmila',	vek: 29, plat: 27500 },
+	{ jmeno: 'Bedřich', vek: 37, plat: 34900 },
+	{ jmeno: 'Zuzana',	vek: 31, plat: 48000 },
+	{ jmeno: 'Eliška',	vek: 42, plat: 32000 },
+	{ jmeno: 'Petr', 		vek: 21, plat: 48000 },
+];
+
+
+
+/*
+13) Použij pole pracovníku z předchozího příkladu
+a vypiš platy všech vykopávek, kterým je přes třicet.
+*/
+
+
+
+/*
+14) Stále pracuj se stejným polem pracovníků.
+Napiš jméno, věk a plat zaměstnance, který vydělává
+ze všech nejvíce.
+*/
+
+
